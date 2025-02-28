69cdf0b37e377db887c753389fbb3c03ab54a57d
nd2k <nicolas.decat@gmail.com>
2025-03-01 00:52:11 +0100

    Modify maven.yml

--- security/src/test/java/com/nd2k/security/SecurityApplicationTests.java
+++ security/src/test/java/com/nd2k/security/SecurityApplicationTests.java
@@ -1,13 +1,9 @@
 package com.nd2k.security;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
